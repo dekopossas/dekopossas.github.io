7fff92934533d1f2b0854cb93bb3a8767ebedf42
DekoPossas <andrepossaster@gmail.com>
2020-08-10 18:54:52 -0300

    criei uma estrutura de repeticao com a contagem regreciva!

---
+++ exercises/7_1/ex.js
@@ -0,0 +1,9 @@
+// aqui eu declarei minha função em formato arrow.
+const fatorial = (n) => {
+    // aqui eu vou fazer uma estrutura de repetição que vai passar pelos numeros de N ate 0.
+    for (let c = n; c > 0; c -= 1){
+        console.log (c);
+    }
+}
+// vou chamar a minha função espero receber uma impreção de 6 numero.
+fatorial(6)
