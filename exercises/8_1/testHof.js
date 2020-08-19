203c2a27e7da4e5d0119236898c2e0a3e2aa6613
DekoPossas <andrepossaster@gmail.com>
2020-08-19 17:59:01 -0300

    versionando meu codigo

--- exercises/8_1/testHof.js
+++ exercises/8_1/testHof.js
@@ -31,3 +31,11 @@ console.log(verifyEven(14)); // True
 const isEven2 = numbers.find((number) => number % 2 === 0);
 
 console.log(isEven2); // 30
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
