17b65eaccfd6693c5ba2a5fc4da7e450482b972f
DekoPossas <andrepossaster@gmail.com>
2020-08-31 09:55:14 -0300

    instalei o pacote json e continuo trabalhando com promises

--- exercises/9_2/promises.js
+++ exercises/9_2/promises.js
@@ -1,11 +1,13 @@
+const fetch = require('node-fetch');
 
-const promise = new Promise((resolve, reject) => {
-    const number = Math.floor(Math.random() * 11);
-  
-    if (number > 10 || number <= 5) {
-      return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+function verifiedFetch(url) {
+  return new Promise((resolve, reject) => {
+    if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+      fetch(url)
+        .then((r) => r.json())
+        .then((r) => resolve(r.value));
+    } else {
+      reject(new Error('endpoint não existe'));
     }
-    resolve(number);
-  })
-  .then(number => `Que sucesso =) nosso número foi ${number}`)
-  .then(msg => console.log(msg));
+  });
+}
