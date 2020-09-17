79091c9db2959993cffd42c701a20cf09ec50ce6
DekoPossas <andrepossaster@gmail.com>
2020-09-17 12:14:59 -0300

    entendo o metodo this para acessar informacoes da pagina

--- exercises/12_1/fancy-buttons/src/App.js
+++ exercises/12_1/fancy-buttons/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function handleclick1() {
-  console.log('Clicou no botão 1');
-}
 function handleclick2() {
   console.log('Clicou no botão 2');
 }
@@ -13,9 +9,12 @@ function handleclick3() {
 }
 
 class App extends React.Component {
+  handleclick1() {
+    console.log('Clicou no botão 1');
+  }
   render(){
     return <div>
-      <button onClick={handleclick1}>My Button1</button>;
+      <button onClick={this.handleclick1}>My Button1</button>;
       <button onClick={handleclick2}>My Button2</button>;
       <button onClick={handleclick3}>My Button3</button>;
       
