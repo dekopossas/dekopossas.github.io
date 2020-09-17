bffef7084c8c1ec8f5d89901d8e2f600058d4162
DekoPossas <andrepossaster@gmail.com>
2020-09-17 17:13:32 -0300

    setei um botao com a function bind para acesso do this

--- exercises/12_1/fancy-buttons/src/App.js
+++ exercises/12_1/fancy-buttons/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function handleclick2() {
-  console.log('Clicou no botão 2');
-}
-function handleclick3() {
-  console.log('Clicou no botão 3');
-}
-
 class App extends React.Component {
-  handleclick1() {
-    console.log('Clicou no botão 1');
+  constructor() {
+    super()
+    this.handleclick = this.handleclick.bind(this)
   }
-  render(){
-    return <div>
-      <button onClick={this.handleclick1}>My Button1</button>;
-      <button onClick={handleclick2}>My Button2</button>;
-      <button onClick={handleclick3}>My Button3</button>;
-      
-      </div>
+
+  handleclick() {
+    console.log('Clicou');
+  }
+  render() {
+    return <button onClick={this.handleclick}>My Button1</button>;
   }
 }
 
