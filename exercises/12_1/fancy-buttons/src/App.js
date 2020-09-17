c4d09501b7e3c851dc0f4edac4b858e316b5adaa
DekoPossas <andrepossaster@gmail.com>
2020-09-16 23:12:24 -0300

    criei os outros 2 botoes

--- exercises/12_1/fancy-buttons/src/App.js
+++ exercises/12_1/fancy-buttons/src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function handleclick() {
-  console.log('Clicou no botão');
+function handleclick1() {
+  console.log('Clicou no botão 1');
+}
+function handleclick2() {
+  console.log('Clicou no botão 2');
+}
+function handleclick3() {
+  console.log('Clicou no botão 3');
 }
 
 class App extends React.Component {
   render(){
-    return <button onClick={handleclick}>My Button</button>;
+    return <div>
+      <button onClick={handleclick1}>My Button1</button>;
+      <button onClick={handleclick2}>My Button2</button>;
+      <button onClick={handleclick3}>My Button3</button>;
+      
+      </div>
   }
 }
 
