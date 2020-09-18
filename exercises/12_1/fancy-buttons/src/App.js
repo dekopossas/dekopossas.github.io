156a35569c81641acf3972361921a34c53bf751d
DekoPossas <andrepossaster@gmail.com>
2020-09-18 15:42:01 -0300

    lidando com contador de botoes

--- exercises/12_1/fancy-buttons/src/App.js
+++ exercises/12_1/fancy-buttons/src/App.js
@@ -4,14 +4,42 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
-    this.handleclick = this.handleclick.bind(this)
+    this.handleclick1 = this.handleclick1.bind(this)
+    this.state = {
+      numeroDeClicks: 0
+    }
+    this.handleclick2 = this.handleclick2.bind(this)
+    this.state = {
+      numeroDeClicks: 0
+    }
+    this.handleclick3 = this.handleclick3.bind(this)
+    this.state = {
+      numeroDeClicks: 0
+    }
   }
 
-  handleclick() {
-    console.log('Clicou');
+  handleclick1() {
+    this.setState((estado, _props) => ({
+      numeroDeClicks: estado.numeroDeClicks + 1
+    }))
+  }
+  handleclick2() {
+    this.setState((estado, _props) => ({
+      numeroDeClicks: estado.numeroDeClicks + 1
+    }))
+  }
+  handleclick3() {
+    this.setState((estado, _props) => ({
+      numeroDeClicks: estado.numeroDeClicks + 1
+    }))
   }
   render() {
-    return <button onClick={this.handleclick}>My Button1</button>;
+    console.log(this)
+    return <div>
+      <button onClick={this.handleclick1}>{this.state.numeroDeClicks}</button>;
+      <button onClick={this.handleclick2}>{this.state.numeroDeClicks}</button>;
+      <button onClick={this.handleclick3}>{this.state.numeroDeClicks}</button>;
+      </div>
   }
 }
 
