c2e5ae3c604e8e24b8ec465732d255c4cdc8fb2e
DekoPossas <andrepossaster@gmail.com>
2020-09-20 01:01:20 -0300

    adicionei uma msg irada de erro!

--- exercises/12_22/forms/src/Cidade.js
+++ exercises/12_22/forms/src/Cidade.js
@@ -3,6 +3,10 @@ import React from 'react';
 class Cidade extends React.Component {
   render() {
     const { value, handleChange } = this.props;
+
+    let error = undefined;
+    if(value === 'value0') error = "Selecione essa opção"
+
     return (
       <label for="cidade">
         Cidade:
@@ -17,6 +21,9 @@ class Cidade extends React.Component {
           <option value="value2">rj</option>
           <option value="value3">sp</option>
         </select>
+        <span>
+          {error ? error : ''}
+        </span>
       </label>
     );
   }
