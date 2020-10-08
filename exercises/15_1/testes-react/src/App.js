3972bd48fb666a2057112357cac04d91ed567e1e
DekoPossas <andrepossaster@gmail.com>
2020-10-08 19:10:04 -0300

    criei uma aplicacao ja com o teste instalado, coloquei um input de email e estou testando esse input

---
+++ exercises/15_1/testes-react/src/App.js
@@ -0,0 +1,17 @@
+    // App.js
+    import React from 'react';
+    import './App.css';
+    
+    function App() {
+      return (
+        <div className="App">
+          <label htmlFor="id-email">
+            Email
+          </label>
+          <input id="id-email" type="email" />
+        </div>
+      );
+    }
+    
+    export default App;
+    
