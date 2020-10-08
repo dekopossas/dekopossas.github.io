80b14c60ad15cd674592215f93ceafa609ebd4ce
DekoPossas <andrepossaster@gmail.com>
2020-10-08 19:14:01 -0300

    adicionei o segundo botton e no teste fiz as alteracoes da forma correta

--- exercises/15_1/testes-react/src/App.js
+++ exercises/15_1/testes-react/src/App.js
@@ -10,6 +10,7 @@
           </label>
           <input id="id-email" type="email" />
           <input id="btn-send" type="button" value="Enviar" />
+          <input id="btn-back" type="button" value="Voltar" />
         </div>
       );
     }
