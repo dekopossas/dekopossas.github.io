47181e07a5ee19c706e2d085005359214ac0b4af
DekoPossas <andrepossaster@gmail.com>
2020-10-08 19:12:36 -0300

    adicionei o teste do botton

--- exercises/15_1/testes-react/src/App.test.js
+++ exercises/15_1/testes-react/src/App.test.js
@@ -1,12 +1,17 @@
-    // App.test.js
-    import React from 'react';
-    import { render } from '@testing-library/react';
-    import App from './App';
-    
-    test('Verificando se existe o campo Email.', () => {
-      const { getByLabelText } = render(<App />);
-      const inputEmail = getByLabelText('Email');
-      expect(inputEmail).toBeInTheDocument();
-      expect(inputEmail.type).toBe('email');
-    });
-    
+// App.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Verificando se existe o campo Email.', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+test('Verificando se existe um botão', () => {
+  const { getByRole } = render(<App />);
+  const btn = getByRole('button');
+  expect(btn).toBeInTheDocument();
+});
