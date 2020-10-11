ceae1d6bde36857e4784253e775f1ed2586341da
DekoPossas <andrepossaster@gmail.com>
2020-10-11 17:35:06 -0300

    coloquei todos as informacoes do mesmo arquivo

--- exercises/revisaoJest/exFixacao.test.js
+++ exercises/revisaoJest/exFixacao.test.js
@@ -1,2 +1,8 @@
-const { isDivisible, randomNumber } = require('./exFixacao');
+const randomNumber = () => Math.round(Math.random()*100)
 
+const isDivisible = (n) => (randomNumber() % n) === 0
+
+test('testando se randomNumber foi chamado em isDivisible'), () => {
+  isDivisible(5);
+  expect(randomNumber).toHaveBeenCalled();
+}
