c6f4e94f521a8103af8c73b0a9122b957ecd8899
DekoPossas <andrepossaster@gmail.com>
2020-10-12 13:58:29 -0300

    terminei de implementar o spyOn

--- exercises/revisaoJest/mockDeModulos.js
+++ exercises/revisaoJest/mockDeModulos.js
@@ -1,4 +1,4 @@
-const somar = async (a, b) => { await sleep(10000); return a + b }; // Função de somar mais lenta do mundo
+const somar = (a, b) =>  a + b ; // Função de somar mais lenta do mundo
 const subtrair = (a, b) => a - b;
 const multiplicar = (a, b) => a * b;
 const dividir = (a, b) => a / b;
--- exercises/revisaoJest/mockDeModulos.test.js
+++ exercises/revisaoJest/mockDeModulos.test.js
@@ -1,14 +1,12 @@
 const math = require('./mockDeModulos');
-jest.mock("./mockDeModulos");
 
 test("#somar", () => {
-  // Aqui testamos se função foi chamada, quantas vezes foi chamada, quais parâmetros foram usados e qual seu retorno
+  // testando se a função foi chamada, quantas vezes foi chamada, quais parâmetros foram usados e qual seu retorno
+  const mockSomar = jest.spyOn(math, "somar");
 
-  math.somar.mockImplementation((a, b) => a + b);
   math.somar(1, 2);
-
-  expect(math.somar).toHaveBeenCalled();
-  expect(math.somar).toHaveBeenCalledTimes(1);
-  expect(math.somar).toHaveBeenCalledWith(1, 2);
-  expect(math.somar(1, 2)).toBe(3);
+  expect(mockSomar).toHaveBeenCalled();
+  expect(mockSomar).toHaveBeenCalledTimes(1);
+  expect(mockSomar).toHaveBeenCalledWith(1, 2);
+  expect(mockSomar(1, 2)).toBe(3);
 });
