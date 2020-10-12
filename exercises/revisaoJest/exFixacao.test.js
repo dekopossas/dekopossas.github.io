3a6c5d121712e798ae8ddd9ae4d336cfea10911a
DekoPossas <andrepossaster@gmail.com>
2020-10-12 12:30:48 -0300

    modifiquei os divisores para testar o mockReturnValueOnce

--- exercises/revisaoJest/exFixacao.test.js
+++ exercises/revisaoJest/exFixacao.test.js
@@ -7,9 +7,12 @@ function isDivisible(n) {
 }
 test('testando se randomNumber foi chamado em isDivisible', () => {
 
-  randomNumber = jest.fn();
+  randomNumber = jest
+    .fn()
+    .mockReturnValueOnce(5)
+    .mockReturnValueOnce(21)
 
-  isDivisible(2);
-
+  isDivisible(3);
   expect(randomNumber).toHaveBeenCalledTimes(1);
+  expect(isDivisible(7)).toBe(true);
 });
