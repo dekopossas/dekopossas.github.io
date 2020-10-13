0d7aafe97816aaa50c3d674457489886cf34fb3e
DekoPossas <andrepossaster@gmail.com>
2020-10-13 16:05:40 -0300

    exercitando os mocks das functions subtrair e multiplicar

--- exercises/revisaoJest/fixacao.test.js
+++ exercises/revisaoJest/fixacao.test.js
@@ -1,1 +1,21 @@
 const math = require('./mockDeModulos');
+
+describe('testando function subtrair', () => {
+  it('chamando subtrair', () => {
+    math.subtrair = jest.fn();
+    math.subtrair();
+    expect(math.subtrair).toHaveBeenCalled();
+  })
+})
+
+describe('testando function multiplicar', () => {
+  it('implementando o retorno padrao em 10', () => {
+    math.multiplicar = jest
+      .fn()
+      .mockReturnValue(10)
+
+    math.multiplicar();
+    expect(math.multiplicar).toHaveBeenCalled();
+    expect(math.multiplicar()).toBe(10)
+  })
+})
