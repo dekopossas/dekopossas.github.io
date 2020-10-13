8c209fda724b02281cf217fcac0ce8c9c307066c
DekoPossas <andrepossaster@gmail.com>
2020-10-13 15:35:30 -0300

    trabalahndo com spyON e mockRestore

--- exercises/revisaoJest/mockDeModulos.test.js
+++ exercises/revisaoJest/mockDeModulos.test.js
@@ -2,11 +2,17 @@ const math = require('./mockDeModulos');
 
 test("#somar", () => {
   // testando se a função foi chamada, quantas vezes foi chamada, quais parâmetros foram usados e qual seu retorno
-  const mockSomar = jest.spyOn(math, "somar");
+  expect(math.somar(1, 2)).toBe(3);
+  const mockSomar = jest
+    .spyOn(math, "somar")
+    .mockImplementation((a, b) => a - b);
 
-  math.somar(1, 2);
-  expect(mockSomar).toHaveBeenCalled();
+  math.somar(5, 1);
   expect(mockSomar).toHaveBeenCalledTimes(1);
-  expect(mockSomar).toHaveBeenCalledWith(1, 2);
-  expect(mockSomar(1, 2)).toBe(3);
+  expect(mockSomar(5, 1)).toBe(4);
+  expect(mockSomar).toHaveBeenCalledTimes(2);
+  expect(mockSomar).toHaveBeenCalledWith(5, 1);
+
+  math.somar.mockRestore();
+  expect(math.somar(1, 2)).toBe(3);
 });
