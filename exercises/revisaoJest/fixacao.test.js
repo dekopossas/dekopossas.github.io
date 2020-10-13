545111de8574c82619287d059e07d2fb048d1d12
DekoPossas <andrepossaster@gmail.com>
2020-10-13 17:26:18 -0300

    criei novas implementacoes da funcao somar

--- exercises/revisaoJest/fixacao.test.js
+++ exercises/revisaoJest/fixacao.test.js
@@ -19,3 +19,20 @@ describe('testando function multiplicar', () => {
     expect(math.multiplicar()).toBe(10)
   })
 })
+
+describe('testando mock da função soma', () => {
+  it('soma com função original', () => {
+    expect(math.somar(1, 2)).toBe(3) 
+  })
+  it('testa um mock da função somar', () => {
+    math.somar = jest.fn().mockImplementation((a, b) => a + b);
+    math.somar(3, 4)
+  })
+  it('se a função somar foi chamada', () => {
+    math.somar(3, 4)
+    expect(math.somar).toHaveBeenCalled()
+  })
+  it('testando se foi somanda a implementation correto', () => {
+    expect(math.somar(4, 3)).toBe(7)
+  })
+})
