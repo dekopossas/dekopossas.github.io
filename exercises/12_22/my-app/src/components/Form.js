2f00f1573147d1655b4959ec1bdd61c636035ff6
DekoPossas <andrepossaster@gmail.com>
2020-10-26 18:53:34 -0300

    completei o forms

--- exercises/12_22/my-app/src/components/Form.js
+++ exercises/12_22/my-app/src/components/Form.js
@@ -1,44 +1,97 @@
-import React, { Component } from 'react'
-
-class Form extends Component {
+import React from 'react';
+class Form extends React.Component {
   constructor() {
-    super();
-
-    this.handleChange = this.handleChange.bind(this);
-
+    super()
+    this.handleChange = this.handleChange.bind(this)
     this.state = {
-      estadoFavorito: '',
-    };
+      name: "",
+      mail: "",
+      cpf: "",
+      adress: "",
+      city: "",
+      state: "",
+      type: "",
+      resume: "",
+      role: "",
+      roleDescription: "",
+    }
   }
-
-
-  handleChange(event) {
+  handleChange({ target }) {
+    const { name } = target
     this.setState({
-      estadoFavorito: event.target.value,
-    });
+      [name]: target.value
+    })
   }
-
+  showContent = () => {
+    [Object.values(this.state)].map((item, key) => {
+      return (
+        <p>{item}</p>
+      )
+    })
+  }
+  clearContent = () => {
+  }
   render() {
     return (
       <div>
-        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
-        <form className="form">
+        <h1>Formulário em React</h1>
+        <fieldset >
           <label>
-            Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+            Nome
+					<input name='name' type='text' onChange={this.handleChange}></input>
           </label>
-          <input
-            type="number"
-            name="idade"
-          />
-          <input
-            type="checkbox"
-            name="vaiComparecer"
-          />
-        </form>
+          <br />
+          <label>
+            E-mail
+					<input name='mail' type='text' onChange={this.handleChange}></input>
+          </label>
+          <br />
+          <label>
+            CPF
+					<input name='cpf' type='text' onChange={this.handleChange}></input>
+          </label>
+          <br />
+          <label>
+            Endereço
+					<input name='adress' type='text' onChange={this.handleChange}></input>
+          </label>
+          <br />
+          <label>
+            Cidade
+					<input name='city' type='text' onChange={this.handleChange}></input>
+          </label>
+          <br />
+          <label>
+            Estado
+					<input name='state' type='ComboBox' onChange={this.handleChange}></input>
+          </label>
+          <br />
+          <label>
+            Tipo
+					<input name='type' type='radio' onChange={this.handleChange}></input>
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            Resumo do Currículo
+					<textarea name='resume' type='textArea' onChange={this.handleChange}></textarea>
+          </label>
+          <br />
+          <label>
+            Cargo
+					<input name='role' type='text' onChange={this.handleChange}></input>
+          </label>
+          <br />
+          <label>
+            Descrição do Cargo
+						<textarea name='roleDescription' type='textArea' onChange={this.handleChange}></textarea>
+          </label>
+        </fieldset>
+        <button onClick={this.showContent}>Submeter</button>
+        {/* exibir aqui */}
+        <button onClick={this.clearContent}>Limpar</button>
       </div>
-    );
+    )
   }
 }
-
 export default Form;
---
+++ exercises/12_22/my-app/src/reducer/index.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = {};
+
+function handleFormChange(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case 'CHANGE':
+      return {}
+  }
+}
