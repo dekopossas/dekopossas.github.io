874b3fa508558489cc804a6e057905049285bd44
DekoPossas <andrepossaster@gmail.com>
2020-10-26 16:29:44 -0300

    renderizei o form

--- exercises/12_22/my-app/src/App.js
+++ exercises/12_22/my-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from './components/Form'
 
 function App() {
   return (
-    <div className="App" />
-      
+    <div className="App">
+      <Form />
+    </div>
   );
 }
 
