b852d94d057a05ab09bf928abaa81954d3710678
deko possas <andrepossaster@gmail.com>
2021-04-26 22:24:05 -0300

    sei la q isso

--- exercises/26_2/readFile.js
+++ exercises/26_2/readFile.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 
-fs.readFile('./arquivo.txt', (err, content) => {
-  if (err) {
-    console.error(`Erro ao ler o arquivo: ${err.message}`);
-    return;
-  }
+fs.readFile('file1.txt', (err, file1Content) => {
+  if (err) return console.log(`Erro ao ler arquivo 1: ${err.message}`);
 
-  console.log(`Arquivo lido com sucesso. Conteúdo: ${content.toString('utf8')}`);
+  console.log(`Lido file1.txt com ${file1Content.byteLength} bytes`);
+
+  fs.readFile('file2.txt', (err, file2Content) => {
+    if (err) return console.log(`Erro ao ler o arquivo 2: ${err.message}`);
+
+    console.log(`Lido file2.txt com ${file2Content.byteLength} bytes`);
+
+    fs.readFile('file3.txt', (err, file3Content) => {
+      if (err) return console.log(`Erro ao ler o arquivo 3: ${err.message}`);
+
+      console.log(`Lido file3.txt com ${file3Content.byteLength} bytes`);
+    });
+  });
 });
