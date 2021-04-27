b1b1400ed07fe50f55847c06338d996ad1634a20
deko possas <andrepossaster@gmail.com>
2021-04-27 15:12:43 -0300

    timestamps e para informar que nao vai ser utilizado

--- exercises/sequelize/models/storeModel.js
+++ exercises/sequelize/models/storeModel.js
@@ -2,5 +2,8 @@ const Store = (sequelize, DataTypes) => {
   const Store = Sequelize.define("Store", {
     name: DataTypes.STRING,
     description: DataTypes.STRING,
+  }, {
+    timestamp: false,
   })
-}
+  return Store;
+};
