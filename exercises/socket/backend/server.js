299aec75dadb65c5493bd08729199a267716b27e
deko possas <andrepossaster@gmail.com>
2021-05-15 18:34:04 -0300

    listiner de socket

--- exercises/socket/backend/server.js
+++ exercises/socket/backend/server.js
@@ -5,6 +5,10 @@ const httpServer = require('http').createServer(app);
 // colocar o cors mais tarde
 const io = require('socket.io')(httpServer);
 
+io.on('connection', (socket) => {
+  console.log('Usuario novo conectado');
+})
+
 app.set('view engine', 'ejs');
 
 app.get('/', (_req, res) => {
